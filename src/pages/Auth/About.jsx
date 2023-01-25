0d0a4a73da54ea938bbd835b557f0eea074e2fbf
apurba2001 <apurbaruidas0358@gmail.com>
2023-01-26 02:33:19 +0530

    Auth page created

---
+++ src/pages/Auth/About.jsx
@@ -0,0 +1,21 @@
+import './Auth.css'
+
+const About = () =>{
+     return(
+        <div className="auth-container1">
+            <h1>Join the stack overflow community</h1>
+            <p>Get unstuck - ask a question</p>
+            <p>Unlock new privileges like voting and commetion</p>
+            <p>Save your favourite tags, filter, and jobs</p>
+            <p>Earn reputation and badges</p>
+            <p style={{ fontSize: "13px", color: "#666767"}}>
+                Collaborate and share knowledge with a private group for
+            </p>
+             <p style={{ fontSize: "13px", color: "#007ac6" }}>
+                Get Stack Overflow for Teams free for up yo 50 users
+            </p>
+        </div>
+     )
+}
+
+export default About
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,7 +1,9 @@
 import './Auth.css'
 import { useState } from 'react'
+import About from './About'
 
 import logo from '../../assets/stack-overflow.png'
+
 
 const Auth = () => {
     const [isSignup, setIsSignup] = useState(false)
@@ -12,34 +14,67 @@ const Auth = () => {
 
     return (
         <section className='auth-section'>
-            <div className='auth-container'>
+            { isSignup && <About/> }
+            <div className='auth-container2'>
                 {!isSignup &&
                     <img src={logo} alt="stack overflow" className='login-logo' />
                 }
                 <form>
+                    {
+                        isSignup && (
+                            <label htmlFor='name'>
+                                <h4>Display name</h4>
+                                <input type="text" id="name" name="name" />
+
+                            </label>
+                        )
+                    }
                     <label htmlFor='username'>
                         <h4>Email</h4>
                         <input type="email" id='username' name='username' />
                     </label>
                     <label htmlFor='password'>
-                        <div>
-                            <h4>Password</h4>
-                            <h4>forgot password</h4>
+                        <div style={{display: "flex", justifyContent: "space-between" }}>
+                            <h4 >Password</h4>
+                            {
+                                !isSignup && <p className='forget-password' style={{ color: "#007ac6", fontSize: "13px" }}>Forgot password?</p>
+                            }
                         </div>
-                        <input type="password" id='password'
-                            name='password'
-                        />
+                        <input type="password" id='password' name='password' />
+                        {isSignup && <p style={{ color: "#0666767", fontSize: "13px", color: "#666767", paddingTop: "5px" }}w>Password must contain at least eight<br />
+                            characters, including at least 1 letter 1 <br/> ber.
+                        </p>}
                     </label>
+                    {
+                        isSignup && (
+                            <label htmlFor='check'>
+                                <input type="checkbox" id="check" style={{ width: "13px", paddingBottom: "15px" }} />
+                                <p style={{ fontSize: "12px", marginTop: "15px", paddingLeft:"10px" }}>Opt-in to receive occasional,<br /> product updates,
+                                    user reserch invitations,<br />
+                                    company announcements, and digests.
+                                </p>
+
+                            </label>
+                        )
+                    }
                     <button type='submit' className='auth-btn'>
-                        {
-                            isSignup ? "Sign up" : "Log in"
-                        }
+                        { isSignup ? "Sign up" : "Log in" }
                     </button>
+                    {
+                        isSignup && (
+                            <p style={{ color: "#0666767", fontSize: "12px", marginTop: "6px"}}>
+                                By clicking "Sign up", you agree to our
+                                <span style={{ color: "#007ac6" }}> terms of <br />service</span>
+                                , <span style={{ color: "#007ac6" }}>privacy privacy</span> and
+                                <span style={{ color: "#007ac6" }}> cookie policy</span>
+                            </p>
+                        )
+                    }
                 </form>
-                <p>
+                <p className='switch-auth'>
                     {
                         isSignup ?
-                            "already have an account" :
+                            "Already have an account? " :
                             "Don't have an account? "
                     }
                     <button
