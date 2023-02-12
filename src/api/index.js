cf7e24d6b45e02fe84c0712b95bca83fea63a9c6
apurba2001 <apurbaruidas0358@gmail.com>
2023-02-13 00:31:57 +0530

    .toml file added

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const SERVER = 'https://stack-overflow-clone-7mrq.onrender.com' 
+const LOCAL = 'http://localhost:5000'
+const API = axios.create({ baseURL: SERVER })
 
 export const login = (authData) => API.post('/user/login', authData)
 export const signup = (authData) => API.post('/user/signup', authData)
